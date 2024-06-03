cdc64730f5c0c5aec32330748a18b7266b0d187b
gulou <gustavosiqueiraloureiro@gmail.com>
2024-06-03 11:36:06 -0300

    Criada as classe Comprar e feitos para as classe Mercado.

---
+++ src/view/Comprar.java
@@ -0,0 +1,71 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Comprar {
+
+    public static ArrayList<Produto> carrinho = new ArrayList<>();
+    public static ArrayList<Produto> estoque = Estoque.produtos();
+
+    public static void AdicionarAoCarrinho(String nome){
+        Produto produto = null;
+        for (Produto p: estoque){
+            if (p.getNome().equals(nome)){
+                produto = p;
+                break;
+            }
+        }
+
+        //Verifica se o produto foi encontrado no estoque
+        if (produto != null) {
+            // Verifica se a quantidade solicitada está disponível no estoque
+            if (produto.getQuantidade() >= 1) {
+
+                // Adiciona o produto ao carrinho
+                //Verefica se o produto já está no carrinho
+                boolean produtoEncontrado = false;
+                for (Produto p: carrinho) {
+                    if (p.getNome().equals(nome)) {
+                        p.setQuantidade(p.getQuantidade());
+                        produtoEncontrado = true;
+                        break;
+                    }
+                }
+                //Se o produto não estiver no carrinho, adiciona um novo produto
+                if (!produtoEncontrado){
+                    Produto produtoCarrinho = new Produto(produto.getNome(), produto.getMarca(),
+                            produto.getPreco(), 1);
+                    carrinho.add(produtoCarrinho);
+                    System.out.println("Produto adicionado ao carrinho com sucesso!");
+                }else{
+                    System.out.println("Produto já se encontra no carrinho");
+                }
+                // Diminui a quantidade do produto no estoque
+                produto.setQuantidade(produto.getQuantidade() - 1);
+
+                // Verifica se a quantidade do produto no estoque se tornou 0 e remove-o se necessário
+                if (produto.getQuantidade() == 0) {
+                    estoque.remove(produto);
+                }
+            } else {
+                System.out.println("Quantidade solicitada não disponível no estoque.");
+            }
+        } else if (produto == null){
+            System.out.println("Produto não encontrado no estoque." +
+                    "\nPor favor verifique a lista de produtos para mais informações");
+        }
+    }
+
+    public static void ListarCarrinho(ArrayList<Produto> carrinho) {
+        double total = 0;
+        for (int i = 0; i < carrinho.size(); i++) {
+            total += carrinho.get(i).getPreco();
+            System.out.println(carrinho.get(i).toString());
+            System.out.println();
+        }
+        System.out.println("O total da compra é de  R$: " + total);
+    }
+
+}
+
--- src/view/Mercado.java
+++ src/view/Mercado.java
@@ -1,14 +1,111 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import view.Produto;
+import view.Estoque;
+import view.Comprar;
+
 public class Mercado {
 
-    private static void menu(){
-        System.out.println("Sejam bem-vindos ao mercado digital.");
-        System.out.println("Por favor selecione qual opção você deseja");
-        System.out.println("1 - Cadastrar produtos");
-        System.out.println("2 - Listar produtos");
-        System.out.println("3 - Comprar produto");
-        System.out.println("4 - Visualizar carrinho");
-        System.out.println("5 - Sair do Sistema");
+    public static void Menu() {
+        Scanner input = new Scanner(System.in);
+        int opcoes = 0;
+
+        ArrayList<Produto> estoque = Comprar.estoque;
+        ArrayList<Produto> carrinho = Comprar.carrinho;
+
+
+        do {
+            System.out.println("Sejam bem-vindos ao mercado digital.");
+            System.out.println("Por favor selecione qual opção você deseja?");
+            System.out.println("1 - Cadastrar produtos");
+            System.out.println("2 - Lista de produtos");
+            System.out.println("3 - Comprar produto");
+            System.out.println("4 - Visualizar carrinho");
+            System.out.println("5 - Finalizar a compra");
+            System.out.println("6 - Sair do Sistema");
+
+            opcoes = input.nextInt();
+            input.nextLine();
+
+            switch (opcoes) {
+                case 1:
+                    System.out.println("Você Escolheu o cadastro de produtos");
+                    Estoque.novoProduto(estoque);
+                    break;
+                case 2:
+                    System.out.println("Você escolheu ver todos os produtos em Estoque");
+                    Estoque.ListeEstoque(estoque);
+                    break;
+                case 3:
+                    boolean continuarComprando = true;
+
+                    do{
+                        System.out.println("Você escolheu comprar produtos. " +
+                                "\nPor favor diga o nome do produto que você deseja comprar");
+
+                        Comprar.AdicionarAoCarrinho(String.valueOf(input.nextLine()));
+                        System.out.println("O que você deseja fazer em seguida?");
+                        System.out.println("1 - Continuar comprando");
+                        System.out.println("2 - Finalizar compra e sair do sistema");
+                        System.out.println("3 - Trocar de Serviço");
+
+                        Scanner scanner = new Scanner(System.in);
+                        int opcao = scanner.nextInt();
+                        switch (opcao){
+                            case(1):
+                                continuarComprando = true;
+                                break;
+                            case (2):
+                                Comprar.ListarCarrinho(carrinho);
+                                continuarComprando = false;
+                                opcoes = 6;
+                                break;
+                            case(3):
+                                continuarComprando = false;
+                                break;
+                            default:
+                                continuarComprando = true;
+                                System.out.println("Opção invalida, por favor selecione uma opção valida");
+                                System.out.println("1 - Continuar comprando");
+                                System.out.println("2 - Finalizar compra e sair do sistema");
+                                break;
+                        }
+                    }while (continuarComprando);
+                    break;
+                case 4:
+                    System.out.println("Você escolheu ver o seu carrinho");
+                    Comprar.ListarCarrinho(carrinho);
+                    break;
+                case 5:
+                    System.out.println("Você escolheu finalizar sua(s) compra(s)");
+
+                    if(carrinho.isEmpty()){
+                        System.out.println("O carrinho está vazio");
+                        System.out.println("Deseja ver meus produtos? (s/n)");
+                        String continuar = input.next();
+                        if (continuar.equalsIgnoreCase("s")) {
+                            opcoes = 2;
+                        }else {
+                            System.out.println("Obrigado por visitar minha loja volte sempre");
+                            opcoes = 6;
+                        }
+
+                    }else{
+                        Comprar.ListarCarrinho(carrinho);
+                        opcoes = 6;
+                    }
+                    break;
+                case 6:
+                    System.out.println("Obrigado por escolher minha loja fazer suas compras, até a próxima");
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }
+        } while (opcoes != 6);
+        System.exit(0);
     }
 }
+
